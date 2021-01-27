c48566dc4223e2b32290d0d67e937fa9e805fdd9
Marek Szlagor <mszlagor@remitly.com>
2021-01-27 13:32:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-args="--regex --rules regexes.json --entropy=False"
+args="--regex --rules regexes.json --entropy=False ${@}"
 
 if [ -n "${INPUT_BRANCH}" ]; then
   args="${args} --branch ${INPUT_BRANCH}"
 fi
+
+echo "${args}"
 
 cp /regexes.json .
 /usr/local/bin/trufflehog ${args} .
